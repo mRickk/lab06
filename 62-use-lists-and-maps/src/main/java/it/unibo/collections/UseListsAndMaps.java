b0af3eea17f1273de25c74c811ddef0941a32569
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2022-10-28 16:04:09 +0200

    Finito es.2

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,7 +1,11 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Example class using {@link List} and {@link Map}.
@@ -19,7 +23,8 @@ public final class UseListsAndMaps {
     public static void main(final String... s) {
         final int START = 1000;
         final int FINISH = 2000;
-        final int TEST = 100_000;        
+        final int N_TEST_ADD = 100_000;
+        final int N_TEST_MIDDLE = 1000;
         /*
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
@@ -40,8 +45,8 @@ public final class UseListsAndMaps {
          * (Suggestion: use a temporary variable)
          */
         final int temp = l1.get(0);
-        l1.set(0, l1.get(l1.size - 1));
-        l1.set(l1.size - 1, (Integer)temp);
+        l1.set(0, l1.get(l1.size() - 1));
+        l1.set(l1.size() - 1, (Integer)temp);
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
@@ -55,14 +60,16 @@ public final class UseListsAndMaps {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
-        testPerformance(TEST, l1, true);
-        testPerformance(TEST, l2, false);
+        testAdd(N_TEST_ADD, l1, true);
+        testAdd(N_TEST_ADD, l2, false);
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example PerfTest.java.
          */
+        testReadingMiddleElement(N_TEST_MIDDLE, l1, true);
+        testReadingMiddleElement(N_TEST_MIDDLE, l2, false);
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
@@ -79,12 +86,24 @@ public final class UseListsAndMaps {
          *
          * Oceania -> 38,304,000
          */
+        Map<String, Long> m1 = new HashMap<>();
+        m1.put("Africa", 1_110_635_000l);
+        m1.put("Americas", 972_005_000l);
+        m1.put("Antartica", 0l);
+        m1.put("Asia", 4_298_723_000l);
+        m1.put("Europe", 742_452_000l);
+        m1.put("Oceania", 38_304_000l);
         /*
          * 8) Compute the population of the world
          */
+        long sum = 0;
+        for (Long l : m1.values()) {
+            sum += l;
+        }
+        System.out.println("World population: " + sum);
     }
 
-    private void testPerformance(final int ELEMS, List list, final boolean isArrayList) {
+    private static void testAdd(final int N, List<Integer> list, final boolean isArrayList) {
         /*
          * Prepare a variable for measuring time
          */
@@ -92,7 +111,7 @@ public final class UseListsAndMaps {
         /*
          * Run the benchmark
          */
-        for (int i = 1; i <= ELEMS; i++) {
+        for (int i = 1; i <= N; i++) {
             list.add(i);
         }
         /*
@@ -102,9 +121,38 @@ public final class UseListsAndMaps {
         final var millis = TimeUnit.NANOSECONDS.toMillis(time);
         System.out.println(// NOPMD
             "Converting "
-                + ELEMS
-                + " ints to Integer and inserting them in a "
-                + (isArrayList ? "Array" : "Linked")
+                + N
+                + " ints to Integer and inserting them in a"
+                + (isArrayList ? "n Array" : " Linked")
+                + "List took "
+                + time
+                + "ns ("
+                + millis
+                + "ms)"
+        );
+    }
+
+    static void testReadingMiddleElement(final int N, final List<Integer> list, final boolean isArrayList) {
+        /*
+         * Prepare a variable for measuring time
+         */
+        long time = System.nanoTime();
+        /*
+         * Run the benchmark
+         */
+        for (int i = 1; i <= N; i++) {
+            list.get((int)(list.size()/2));
+        }
+        /*
+         * Compute the time and print result
+         */
+        time = System.nanoTime() - time;
+        final var millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println(// NOPMD
+            "Reading "
+                + N
+                + " Integers from a"
+                + (isArrayList ? "n Array" : " Linked")
                 + "List took "
                 + time
                 + "ns ("
